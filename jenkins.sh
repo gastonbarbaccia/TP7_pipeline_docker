2333d0c715699d7f28e6e2dfb60fda320d93b409
Gaston Barbaccia <gastonbarbaccia@hotmail.com>
2023-11-06 12:51:36 -0300

    Se modifica script con los parametros -y

--- jenkins.sh
+++ jenkins.sh
@@ -3,18 +3,18 @@ sudo usermod -a -G docker jenkins
 
 #Install Java
 echo "Installing default-java"
-sudo apt update
-sudo apt install fontconfig openjdk-17-jre
+sudo apt update -y
+sudo apt install fontconfig openjdk-17-jre -y
 
 #Install Jenkins
 echo "Installing Jenkins"
 sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
   https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+sudo echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get update
-sudo apt-get install jenkins
+sudo apt-get update -y
+sudo apt-get install jenkins -y
 
 sleep 1m
 
