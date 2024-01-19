f9c9862641085d922b0c0a3e107d0a4492d45ffc
Gaston Barbaccia <gastonbarbaccia@hotmail.com>
2024-01-19 17:29:16 -0300

    ok

--- Vagrantfile
+++ Vagrantfile
@@ -4,9 +4,15 @@
 Vagrant.configure("2") do |config|
   config.vm.define "master" do |master|
     master.vm.box = "ubuntu/bionic64"
-    master.vm.network "forwarded_port", guest: 8080, host: 8080
+#    master.vm.network "forwarded_port", guest: 8080, host: 8080
+#    master.vm.network "forwarded_port", guest: 80, host: 80
+#    master.vm.network "forwarded_port", guest: 443, host: 443
+    master.vm.network "public_network"
+
     master.vm.provider "virtualbox" do |v|
       v.name = "master"
+      v.memory = "4096"
+      v.cpus = 2
      end
     master.vm.provision "shell" do |s|
       s.path = 'jenkins.sh'
--- jenkins.sh
+++ jenkins.sh
@@ -1,41 +1,29 @@
 #!/bin/bash
-sudo usermod -a -G docker jenkins
 
-#Install Java
-echo "Installing default-java"
-sudo apt update
-sudo apt install fontconfig openjdk-17-jre -y
+sudo apt-get update -y
+sudo apt-get upgrade -y
 
-#Install Jenkins
-echo "Installing Jenkins"
-sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
-  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
-sudo echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-  /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get update
-sudo apt-get install jenkins -y
-
-sleep 1m
-
-echo "Password Jenkins"
-JENKINSPWD=$(sudo cat /var/lib/jenkins/secrets/initialAdminPassword)
-echo $JENKINSPWD
-
-#Install docker
-echo "Installing docker"
-# Add Docker's official GPG key:
-sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg
+#Instalacion de  docker
+sudo apt-get install ca-certificates curl gnupg wget apt-transport-https -y
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
-
-# Add the repository to Apt sources:
 echo \
   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update -y
 
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y 
+
+
+#Descargamos e instalamos los binarios de microk8s
+sudo snap install microk8s --classic --channel=1.29
+
+
+
+#instalacion de argocd
+sudo microk8s kubectl create namespace argocd
+git clone https://github.com/argoproj/argo-cd.git
+sudo microk8s  kubectl apply -n argocd --force -f argo-cd/manifests/install.yaml
+sudo microk8s kubectl get all -n argocd
